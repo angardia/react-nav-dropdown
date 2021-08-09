bdca2df3de4a986f5adbffa4b87af37a85903ab1
angardia <37413438+angardia@users.noreply.github.com>
2021-08-10 00:07:08 +0300

    refactor: remove state & function

--- src/Navbar/NavItem/Navitem.js
+++ src/Navbar/NavItem/Navitem.js
@@ -3,20 +3,11 @@ import "./Navitem.css";
 
 export default function Navitem(props) {
 
-    const [navState, setNavState] = useState(false);
-
-    function toggle() {
-        setNavState((prevState) => !prevState);
-        console.log(navState);
-    }
-
     return (
         <li className="nav-item">
-            <a href="#" className="nav-item-icon" onClick={toggle}>
+            <a href="#" className="nav-item-icon">
                 {props.icon}
             </a>
-
-            {navState && props.children}
         </li>
     )
 }
