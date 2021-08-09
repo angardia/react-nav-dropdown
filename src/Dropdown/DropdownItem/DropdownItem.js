edd1d002fb9bd6a2c4e44c933cd43ea5a7eb44f6
angardia <37413438+angardia@users.noreply.github.com>
2021-08-10 00:12:17 +0300

    refactor: add li wrapper for semantic purpose

--- src/Dropdown/DropdownItem/DropdownItem.js
+++ src/Dropdown/DropdownItem/DropdownItem.js
@@ -3,8 +3,11 @@ import "./DropdownItem.css";
 
 export default function DropdownItem(props) {
     return (
-        <a href="" className="DropdownItem">
-            {props.children}
-        </a>
+        <li>
+            <a href="" className="DropdownItem">
+                {props.children}
+            </a>
+        </li>
+
     )
 }
