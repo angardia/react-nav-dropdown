88985245b1f3591ef9ce905fb48007f764a4ce20
angardia <37413438+angardia@users.noreply.github.com>
2021-08-10 00:20:58 +0300

    refactor: add context hook

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
-import DropdownItem from './DropdownItem/DropdownItem';
+import React, { useState, createContext } from 'react';
+import DropdownList from "./DropdownList/DropdownList";
+import DropdownItem from "./DropdownItem/DropdownItem";
+import DropdownBtn from "./DropdownBtn/DropdownBtn";
+
 import "./Dropdown.css";
+export const Context = createContext();
 
 export default function Dropdown() {
 
-    return (
-        <nav className="Dropdown" >
-          <DropdownItem>test</DropdownItem>
-          <DropdownItem>test</DropdownItem>
-          <DropdownItem>test</DropdownItem>
-        </nav>
-    )
+  const [show, setShow] = useState(false);
+
+  return (
+    <Context.Provider value={{
+      show, setShow
+    }}>
+      <DropdownBtn></DropdownBtn>
+      <DropdownList className="Dropdown" >
+        <DropdownItem>test</DropdownItem>
+        <DropdownItem>test</DropdownItem>
+        <DropdownItem>test</DropdownItem>
+      </DropdownList>
+    </Context.Provider>
+  )
 }
