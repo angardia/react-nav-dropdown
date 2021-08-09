6ce339ab4bd6447baa8d6d745c8cf74d963b76ed
angardia <37413438+angardia@users.noreply.github.com>
2021-08-09 23:58:42 +0300

    refactor: remove value to avoid console error

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {
     return (
         <nav className="Navbar">
             <div className="Navbar-search" >
-                <input className="Navbar_Input" type="text" value="" />
+                <input className="Navbar_Input" type="text" />
                 <img className="Navbar_Icon" alt="search icon" src={require("../icons/search.svg").default} />
             </div>
             <ul className="Navbar-navlist">{props.children}</ul>
